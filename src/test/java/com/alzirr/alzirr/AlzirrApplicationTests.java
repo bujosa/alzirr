e32c53bb2e428e74c26524bea0b36de89fad3630
bujosa <davidbujosa@gmail.com>
2023-11-08 20:21:38 -0400

    feat: spring init

---
+++ src/test/java/com/alzirr/alzirr/AlzirrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alzirr.alzirr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlzirrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
